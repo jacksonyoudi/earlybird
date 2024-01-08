bea4bc10edf6bed1bf4a7357da57ced628f637d5
Changyou Liang <liangchangyoujackson@gmail.com>
2024-01-08 16:19:36 +0800

    var

--- first.playground/Contents.swift
+++ first.playground/Contents.swift
@@ -1,3 +1,12 @@
-var greeting:String = "Hello, playground"
+//打印数据
+// public func print(_ items: Any..., separator: String = " ", terminator: String = "\n")
+print("Hello, world!")
+print(1,2,3,4)
 
-print(1122)
+var myVar: Int = 10
+myVar = 40
+
+print(myVar)
+
+let b :Float = 100.0
+print(b)
